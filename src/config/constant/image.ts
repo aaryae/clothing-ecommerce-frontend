b12689e195a22a3543ccf4f35d13bf6e532f4c2d
aaryae <aaryaed@gmail.com>
2024-07-30 16:33:28 +0545

    feat: product card component

--- src/config/constant/image.ts
+++ src/config/constant/image.ts
@@ -10,6 +10,12 @@ import sectionplate from '@assets/images/landingImages/herosectionplate.webp'
 import herowhite from '@assets/images/landingImages/herosectionwhite.webp'
 import frontmen from '@assets/images/landingImages/test.webp'
 import frontwomen from '@assets/images/landingImages/test2.webp'
+import womenproduct1 from '@assets/images/products/womenproducts/womenproduct1.webp'
+import womenproduct2 from '@assets/images/products/womenproducts/womenproduct2.webp'
+import womenproduct3 from '@assets/images/products/womenproducts/womenproduct3.webp'
+import womenproduct4 from '@assets/images/products/womenproducts/womenproduct4.webp'
+import womenproduct5 from '@assets/images/products/womenproducts/womenproduct5.webp'
+import womenproduct6 from '@assets/images/products/womenproducts/womenproduct6.webp'
 import reviewimage1 from '@assets/images/reviewImages/reviewimage1.webp'
 import reviewimage2 from '@assets/images/reviewImages/reviewimage2.webp'
 import reviewimage3 from '@assets/images/reviewImages/reviewimage3.png'
@@ -29,5 +35,11 @@ export const image = {
   aboutimage1,
   aboutimage2,
   aboutimage3,
+  womenproduct1,
+  womenproduct2,
+  womenproduct3,
+  womenproduct4,
+  womenproduct5,
+  womenproduct6,
   fallback,
 }
---
+++ src/ui/landingPage/organisms/ProductCard.tsx
@@ -0,0 +1,45 @@
+const ProductCard = ({
+  image,
+  productHeading,
+  price,
+  discount = false,
+  originalPrice,
+  discountPrice,
+  productState = false,
+  productValue,
+}: {
+  image: string
+  productHeading: string
+  price: number
+  discount?: boolean
+  originalPrice?: number
+  discountPrice?: number
+  productState?: boolean
+  productValue?: string
+}) => {
+  return (
+    <div className='group relative w-fit cursor-pointer'>
+      <div className='relative'>
+        <img className='relative' src={image} alt='productimg' />
+        <span className='absolute bottom-0 left-0 right-0 bg-[#ad6343]  text-white opacity-0 group-hover:opacity-100 transition-opacity p-2 text-center'>
+          Quick View
+        </span>
+        {productState && (
+          <span className='absolute top-0 bg-[#ad6343]  text-white font-medium px-3 text-sm'>{productValue}</span>
+        )}
+      </div>
+      <p className='font-semibold tracking-widest'>{productHeading}</p>
+      <p className='text-sm tracking-wide text-[#ad6343] font-bold'>
+        {discount && originalPrice !== undefined && discountPrice !== undefined ? (
+          <>
+            <span className='line-through mr-2'>${originalPrice}</span>${discountPrice}
+          </>
+        ) : (
+          <span>${price}</span>
+        )}
+      </p>
+    </div>
+  )
+}
+
+export default ProductCard
